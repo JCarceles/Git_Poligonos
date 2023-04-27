5ab6bce83b2e5f2fa6ab66f589a257b5120b7995
SalahSenhaji <salahgabana@gmail.com>
2023-04-27 12:47:26 +0200

    implementada el circulo

---
+++ src/main/java/com/git_poligonos/Circulo.java
@@ -0,0 +1,17 @@
+package com.git_poligonos;
+
+public class Circulo implements Poligonos {
+
+    @Override
+    public double area() {
+     
+        throw new UnsupportedOperationException("Unimplemented method 'area'");
+    }
+
+    @Override
+    public double perimetro() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'perimetro'");
+    }
+
+}
--- src/main/java/com/git_poligonos/Poligonos.java
+++ src/main/java/com/git_poligonos/Poligonos.java
@@ -1,8 +1,9 @@
 package com.git_poligonos;
 
 public interface Poligonos {
-    
+
     public double area();
+
     public double perimetro();
-    
+
 }
